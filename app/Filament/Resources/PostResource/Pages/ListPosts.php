f680e4fa9be6c72f987c257c3f2a890c5a92efb6
Syamil Taqiyuddin <syamil_t_ayyasy_x@telkomsel.co.id>
2024-09-16 15:27:53 +0700

    Table Tabs

--- app/Filament/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Resources/PostResource/Pages/ListPosts.php
@@ -4,7 +4,12 @@ namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
+use Filament\Forms\Components\Builder;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+
+// use Filament\Resources\Pages\ListRecords\Tab;
 
 class ListPosts extends ListRecords
 {
@@ -14,6 +19,19 @@ class ListPosts extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make(),
+            'Published' => Tab::make()->modifyQueryUsing(function (EloquentBuilder $query) {
+                $query->where('published', true);
+            }),
+            'Unpublished' => Tab::make()->modifyQueryUsing(function (EloquentBuilder $query) {
+                $query->where('published', false);
+            }),
         ];
     }
 }
